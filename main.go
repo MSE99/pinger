e91d082b0176238aab3e5ed5f814cc7a87d45ca4
mse99 <mohamedsalemedrah@gmail.com>
2024-06-04 15:02:21 +0200

    Add a simple Web UI to view the status of services

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
@@ -41,11 +42,15 @@ func main() {
 		startChecker(ctx, def)
 	}
 
-	app := fiber.New()
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		statuses := checkOnAll(conf.Apps)
-		return c.JSON(statuses)
+		results := checkOnAll(conf.Apps)
+		return c.Render("index", results)
 	})
 
 	go func() {
